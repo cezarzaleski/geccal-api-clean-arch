d4519387a7b373ff8364a527de998e17da8fbb60
Cezar Gabriel Heinen Zaleski <cezar.zaleski@gmail.com>
2022-05-17 00:15:45 -0300

    feat: add nestjs

---
+++ src/nestjs/jest.config.ts
@@ -0,0 +1,18 @@
+export default {
+  displayName: {
+    name: 'nestjs',
+    color: 'magentaBright',
+  },
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': '@swc/jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '@geccal/core/(.*)$': '<rootDir>/../../../node_modules/@geccal/core/dist/$1',
+  }
+};
