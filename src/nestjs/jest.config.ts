f7653805a8175db38e0e4c18c75f87615fab9e1b
Cezar Gabriel Heinen Zaleski <97896199+cezarheinen@users.noreply.github.com>
2022-06-18 14:50:57 -0300

    feat: add module loan in core

--- src/nestjs/jest.config.ts
+++ src/nestjs/jest.config.ts
@@ -15,5 +15,6 @@ export default {
   moduleNameMapper: {
     '#shared/(.*)$': '<rootDir>/../../../node_modules/@geccal/core/dist/@shared/$1',
     '#collection/(.*)$': '<rootDir>/../../../node_modules/@geccal/core/dist/collection/$1',
+    '#loan/(.*)$': '<rootDir>/../../../node_modules/@geccal/core/dist/loan/$1',
   }
 };
