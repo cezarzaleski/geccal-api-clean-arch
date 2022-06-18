e3615943a5e7235ae6cdfc8d3418a0c7745818ef
Cezar Gabriel Heinen Zaleski <97896199+cezarheinen@users.noreply.github.com>
2022-06-18 19:33:55 -0300

    feat: add create loan use case

---
+++ src/@core/src/loan/application/use-cases/create-loan.use-case.ts
@@ -0,0 +1,27 @@
+import { default as DefaultUseCase } from '#shared/application/use-case'
+import { LoanOutput, LoanOutputMapper } from '#loan/application/dto/loan-output';
+import LoanRepository from '#loan/domain/repository/loan.repository';
+import { Loan, RegistrationId } from '#loan/domain';
+
+
+export namespace CreateLoanUseCase {
+  export class UseCase implements DefaultUseCase<Input, Output> {
+    constructor(private loanRepository: LoanRepository.Repository) {}
+
+    async execute(input: Input): Promise<Output> {
+      const countPendingLoanRegistration = await this.loanRepository
+        .countLoansPendingByRegistrationId(new RegistrationId(input.registrationId))
+      const loan = Loan.from(input, countPendingLoanRegistration)
+      await this.loanRepository.insert(loan);
+      return LoanOutputMapper.toOutput(loan)
+    }
+  }
+
+  export type Input = {
+    registrationId: string
+    bookId: string
+    borrowedAt: Date
+  };
+
+  export type Output = LoanOutput
+}
