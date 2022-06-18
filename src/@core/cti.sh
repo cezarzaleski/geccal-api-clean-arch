4676c92fb2285c3328003704cb907a6642ab2931
Cezar Gabriel Heinen Zaleski <97896199+cezarheinen@users.noreply.github.com>
2022-06-18 12:09:17 -0300

    refactor: remove book module

--- src/@core/cti.sh
+++ src/@core/cti.sh
@@ -4,10 +4,6 @@ npm run cti create './src/@shared/application' -- -i '*spec.ts' -b &&
 npm run cti create './src/@shared/domain' -- -i '*spec.ts' -b &&
 npm run cti create './src/@shared/infra' -- -i '*spec.ts' -b &&
 
-npm run cti create './src/book/application' -- -i '*spec.ts' -b &&
-npm run cti create './src/book/domain' -- -i '*spec.ts' -b &&
-npm run cti create './src/book/infra' -- -i '*spec.ts' -b &&
-
 npm run cti create './src/collection/application' -- -i '*spec.ts' -b &&
 npm run cti create './src/collection/domain' -- -i '*spec.ts' -b &&
 npm run cti create './src/collection/infra' -- -i '*spec.ts' -b
