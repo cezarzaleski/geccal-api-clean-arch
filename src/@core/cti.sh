e89b9338a1990fd87716177c8eb193d3a40912dc
Cezar Gabriel Heinen Zaleski <97896199+cezarheinen@users.noreply.github.com>
2022-06-19 18:26:03 -0300

    chore: configure exports files with cti library

--- src/@core/cti.sh
+++ src/@core/cti.sh
@@ -2,11 +2,9 @@
 
 npm run cti create './src/@shared/application' -- -i '*spec.ts' -b &&
 npm run cti create './src/@shared/domain' -- -i '*spec.ts' -b &&
-npm run cti create './src/@shared/infra' -- -i '*spec.ts' -b &&
 
 npm run cti create './src/loan/application' -- -i '*spec.ts' -b &&
 npm run cti create './src/loan/domain' -- -i '*spec.ts' -b &&
-npm run cti create './src/loan/infra' -- -i '*spec.ts' -b &&
 
 npm run cti create './src/collection/application' -- -i '*spec.ts' -b &&
 npm run cti create './src/collection/domain' -- -i '*spec.ts' -b &&
---
+++ src/@core/src/collection/infra/db/sequelize/index.ts
@@ -0,0 +1,4 @@
+// created from 'create-ts-index'
+
+export * from './book.model';
+export * from './book.repository.sequelize';
