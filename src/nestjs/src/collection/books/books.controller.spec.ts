e87f8cd0fea18c83d902e4ee5607865fabc73a49
Cezar Gabriel Heinen Zaleski <97896199+cezarheinen@users.noreply.github.com>
2022-06-19 18:54:53 -0300

    test: integrate create book endpoint to create book use case

--- src/nestjs/src/collection/books/books.controller.spec.ts
+++ src/nestjs/src/collection/books/books.controller.spec.ts
@@ -1,5 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { mock, MockProxy } from 'jest-mock-extended';
 import { BooksController } from './books.controller';
+import { CreateBookUseCase } from '@geccal/core/collection/application';
+import { PublisherId } from '@geccal/core/collection/domain';
+import { CreateBookDto } from 'src/collection/books/dto/create-book.dto';
 
 describe('BooksController', () => {
   let controller: BooksController;
@@ -13,7 +17,30 @@ describe('BooksController', () => {
     controller = module.get<BooksController>(BooksController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should create a book', async () => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    const publisherId = new PublisherId().value;
+    const expectedOutput: CreateBookUseCase.Output = {
+      authors: ['luiz', 'marias'],
+      createdAt: new Date(),
+      edition: '1ª',
+      exemplary: 1,
+      origin: 'doacao',
+      publisherId: publisherId,
+      name: 'livro',
+      status: 'disponivel',
+      note: 'note',
+    };
+    const mockCreateUseCase = {
+      execute: jest.fn().mockReturnValue(Promise.resolve(expectedOutput)),
+    };
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    controller['createUseCase'] = mockCreateUseCase;
+    const input: MockProxy<CreateBookDto> = mock();
+    const output = await controller.create(input);
+    expect(mockCreateUseCase.execute).toHaveBeenCalledWith(input);
+    expect(expectedOutput).toStrictEqual(output);
   });
 });
