a0436a37572cd626c5360b8b6994b5d2d6cf4394
Cezar Gabriel Heinen Zaleski <97896199+cezarheinen@users.noreply.github.com>
2022-06-20 23:45:00 -0300

    feat: add available book to borrow service

--- src/@core/src/loan/application/use-cases/borrow-book.use-case.spec.ts
+++ src/@core/src/loan/application/use-cases/borrow-book.use-case.spec.ts
@@ -3,23 +3,26 @@ import { BorrowBookUseCase } from '#loan/application/use-cases/borrow-book.use-c
 import LoanRepository from '#loan/domain/repository/loan.repository';
 import { Loan } from '#loan/domain';
 import { v4 as uuidv4 } from 'uuid';
+import { BookRepository } from '#collection/domain';
 
 
 describe('BorrowBookUseCase Unit test', function () {
 
   let subject: BorrowBookUseCase.UseCase;
-  let repository: MockProxy<LoanRepository.Repository>
+  let loanRepository: MockProxy<LoanRepository.Repository>
+  let bookRepository: MockProxy<BookRepository.Repository>
   let loan: Loan
 
   beforeEach(() => {
-    repository = mock()
-    subject = new BorrowBookUseCase.UseCase(repository);
+    loanRepository = mock()
+    bookRepository = mock()
+    subject = new BorrowBookUseCase.UseCase(loanRepository, bookRepository);
   });
 
   it('given a valid command when call create loan then return id loan', async () => {
-    repository.insert.mockResolvedValue()
-    const spyInsert = jest.spyOn(repository, 'insert');
-    const spycountLoansPendingByRegistrationId = jest.spyOn(repository, 'countLoansPendingByRegistrationId');
+    loanRepository.insert.mockResolvedValue()
+    const spyInsert = jest.spyOn(loanRepository, 'insert');
+    const spycountLoansPendingByRegistrationId = jest.spyOn(loanRepository, 'countLoansPendingByRegistrationId');
     const props = {
       bookId: uuidv4(),
       registrationId: uuidv4(),
@@ -28,7 +31,7 @@ describe('BorrowBookUseCase Unit test', function () {
 
     const output = await subject.execute(props);
 
-    loan = repository.insert.mock.calls[0][0]
+    loan = loanRepository.insert.mock.calls[0][0]
     expect(spyInsert).toHaveBeenCalledTimes(1);
     expect(spycountLoansPendingByRegistrationId).toHaveBeenCalledTimes(1);
     expect(output).toStrictEqual({
--- src/@core/src/loan/application/use-cases/borrow-book.use-case.ts
+++ src/@core/src/loan/application/use-cases/borrow-book.use-case.ts
@@ -2,15 +2,24 @@ import { default as DefaultUseCase } from '#shared/application/use-case'
 import { LoanOutput, LoanOutputMapper } from '#loan/application/dto/loan-output';
 import LoanRepository from '#loan/domain/repository/loan.repository';
 import { Loan, RegistrationId } from '#loan/domain';
+import { BookRepository } from '#collection/domain';
+import AvailableBookService from '#loan/domain/entities/available-book.service';
 
 
 export namespace BorrowBookUseCase {
   export class UseCase implements DefaultUseCase<Input, Output> {
-    constructor(private loanRepository: LoanRepository.Repository) {}
+    constructor(
+      private loanRepository: LoanRepository.Repository,
+      private bookRepository: BookRepository.Repository
+    ) {}
 
     async execute(input: Input): Promise<Output> {
+      const {bookId, registrationId} = input
+      // const book = await this.bookRepository.findById(bookId)
+      // const unavailableBook = !AvailableBookService.available(book)
+      // if (unavailableBook) return
       const countPendingLoanRegistration = await this.loanRepository
-        .countLoansPendingByRegistrationId(new RegistrationId(input.registrationId))
+        .countLoansPendingByRegistrationId(new RegistrationId(registrationId))
       const loan = Loan.from(input, countPendingLoanRegistration)
       await this.loanRepository.insert(loan);
       return LoanOutputMapper.toOutput(loan)
@@ -25,3 +34,6 @@ export namespace BorrowBookUseCase {
 
   export type Output = LoanOutput
 }
+
+//rules
+//book available
