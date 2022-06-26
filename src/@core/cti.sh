14c2a1442504ca2aa9dbd8659496b7064df9f5ff
Cezar Gabriel Heinen Zaleski <97896199+cezarheinen@users.noreply.github.com>
2022-06-26 16:42:16 -0300

    chore: configure enrollment cti.sh

--- src/@core/cti.sh
+++ src/@core/cti.sh
@@ -8,4 +8,8 @@ npm run cti create './src/loan/domain' -- -i '*spec.ts' -b &&
 
 npm run cti create './src/collection/application' -- -i '*spec.ts' -b &&
 npm run cti create './src/collection/domain' -- -i '*spec.ts' -b &&
-npm run cti create './src/collection/infra' -- -i '*spec.ts' -b
+npm run cti create './src/collection/infra' -- -i '*spec.ts' -b &&
+
+npm run cti create './src/enrollment/application' -- -i '*spec.ts' -b &&
+npm run cti create './src/enrollment/domain' -- -i '*spec.ts' -b &&
+npm run cti create './src/enrollment/infra' -- -i '*spec.ts' -b
