653f84a51b5db14343d7bba5c9f85531206ea563
Cezar Gabriel Heinen Zaleski <97896199+cezarheinen@users.noreply.github.com>
2022-06-26 18:06:45 -0300

    feat: add clas validator

---
+++ src/@core/src/enrollment/domain/validators/class.validator.ts
@@ -0,0 +1,46 @@
+import { IsDate, IsNotEmpty, IsString, NotEquals, IsNumber } from 'class-validator';
+import { ClassProperties } from '#enrollment/domain';
+import { ClassValidatorFields } from '#shared/domain';
+
+export class ClassRules {
+  @IsDate()
+  @NotEquals(null)
+  startAt: Date;
+
+  @IsDate()
+  @NotEquals(null)
+  finishAt: Date;
+
+  @IsNumber()
+  @NotEquals(null)
+  year: number;
+
+  @IsString()
+  @IsNotEmpty()
+  ciclo: string;
+
+  constructor(
+    {
+      startAt, finishAt, year, ciclo
+    }: ClassProperties) {
+    Object.assign(this, {
+      startAt: startAt,
+      finishAt: finishAt,
+      year: year,
+      ciclo: ciclo,
+    });
+  }
+}
+
+export class ClassValidator extends ClassValidatorFields<ClassRules> {
+  validate(data: ClassProperties): boolean {
+    return super.validate(new ClassRules(data ?? {} as any));
+  }
+}
+
+
+export class ClassValidatorFactory {
+  static create(): ClassValidator {
+    return new ClassValidator();
+  }
+}
