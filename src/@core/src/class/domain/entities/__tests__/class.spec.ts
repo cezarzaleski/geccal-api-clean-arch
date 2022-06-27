4e8f124f4667d835c80535df8b926f1a521d91c1
Cezar Gabriel Heinen Zaleski <97896199+cezarheinen@users.noreply.github.com>
2022-06-26 21:38:06 -0300

    feat: integrate class validator in the class entity

--- src/@core/src/class/domain/entities/__tests__/class.spec.ts
+++ src/@core/src/class/domain/entities/__tests__/class.spec.ts
@@ -1,5 +1,6 @@
 import ClassPropertiesFake from '#class/domain/entities/__tests__/class-properties.fake';
 import Class from '#class/domain/entities/class';
+import { EntityValidationError } from '#shared/domain';
 
 
 describe('Class Unit Test', () => {
@@ -7,5 +8,19 @@ describe('Class Unit Test', () => {
     const classProps = ClassPropertiesFake.build();
     const subject = Class.from(classProps);
     expect(subject).toBeDefined();
+  });
+
+  test('should be throw EntityValidationError when year is null', () => {
+    const classProps = ClassPropertiesFake.build({year: null});
+    expect(() => {
+      Class.from(classProps);
+    }).toThrow(new EntityValidationError(
+      {
+        year: [
+          'year should not be equal to null',
+          'year must be a number conforming to the specified constraints'
+        ]
+      }
+    ))
   });
 });
--- src/@core/src/class/domain/entities/class.ts
+++ src/@core/src/class/domain/entities/class.ts
@@ -1,5 +1,6 @@
-import { Entity, UniqueEntityId } from '#shared/domain';
+import { Entity, EntityValidationError, UniqueEntityId } from '#shared/domain';
 import Ciclo from '#class/domain/entities/ciclo.vo';
+import { ClassValidatorFactory } from '#class/domain/validators/class.validator';
 
 export type ClassProperties = {
   startAt: Date;
@@ -25,9 +26,16 @@ export default class Class extends Entity {
 
   static from(props: ClassProperties) {
     props.createdAt = props.createdAt ?? new Date();
+    Class.validate(props)
     const {startAt, finishAt, year, ciclo, createdAt} = props;
     const aCiclo = Ciclo.from(ciclo);
     return new Class(startAt, finishAt, year, aCiclo, new UniqueEntityId(), createdAt, new Date());
+  }
+
+  static validate(props: ClassProperties) {
+   const validator = ClassValidatorFactory.create()
+   const isInvalid = !validator.validate(props)
+   if (isInvalid) throw new EntityValidationError(validator.errors)
   }
 
   delete(deletedAt: Date) {
