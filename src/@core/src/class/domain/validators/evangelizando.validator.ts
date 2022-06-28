ed09f14949b19f0e73574c37874cad678b514ef0
Cezar Gabriel Heinen Zaleski <97896199+cezarheinen@users.noreply.github.com>
2022-06-27 22:59:58 -0300

    feat: add validator for evangelizando entity

---
+++ src/@core/src/class/domain/validators/evangelizando.validator.ts
@@ -0,0 +1,51 @@
+import { IsDate, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { ClassValidatorFields } from '#shared/domain';
+import { EvangelizandoProperties } from '#class/domain';
+
+export class EvangelizandoRules {
+  @IsString()
+  @IsNotEmpty()
+  name: String;
+
+  @IsString()
+  @IsNotEmpty()
+  sex: String;
+
+  @IsString()
+  @IsOptional()
+  fatherName: String;
+
+  @IsString()
+  @IsOptional()
+  motherName: String;
+
+  @IsDate()
+  @IsOptional()
+  birthday: Date;
+
+  constructor(
+    {
+      name, sex, fatherName, motherName, birthday
+    }: EvangelizandoProperties) {
+    Object.assign(this, {
+      name: name,
+      sex: sex,
+      fatherName: fatherName,
+      motherName: motherName,
+      birthday: birthday,
+    });
+  }
+}
+
+export class EvangelizandoValidator extends ClassValidatorFields<EvangelizandoRules> {
+  validate(data: EvangelizandoProperties): boolean {
+    return super.validate(new EvangelizandoRules(data ?? {} as any));
+  }
+}
+
+
+export class EvangelizandoValidatorFactory {
+  static create(): EvangelizandoValidator {
+    return new EvangelizandoValidator();
+  }
+}
