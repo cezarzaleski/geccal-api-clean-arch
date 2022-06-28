2e431d0703613f01388b35a1a85f33b156fcb306
Cezar Gabriel Heinen Zaleski <97896199+cezarheinen@users.noreply.github.com>
2022-06-27 23:01:24 -0300

    fix: remove required to fatherName, motherName and birthday fields

--- src/@core/src/class/domain/entities/evangelizando.ts
+++ src/@core/src/class/domain/entities/evangelizando.ts
@@ -2,23 +2,23 @@ import { Entity, UniqueEntityId } from '#shared/domain';
 
 export type EvangelizandoProperties = {
   name: string;
-  fatherName: string;
-  motherName: string;
-  birthday: Date;
   sex: string;
+  fatherName?: string;
+  motherName?: string;
+  birthday?: Date;
   createdAt?: Date;
 }
 
 export default class Evangelizando extends Entity {
   private constructor(
     public name: string,
-    public fatherName: string,
-    public motherName: string,
-    public birthday: Date,
     public sex: string,
     id: UniqueEntityId,
     public readonly createdAt: Date,
     public readonly updateAt: Date,
+    public fatherName?: string,
+    public motherName?: string,
+    public birthday?: Date,
     public deletedAt?: Date,
   ) {
     super(id);
@@ -27,7 +27,7 @@ export default class Evangelizando extends Entity {
   static from(props: EvangelizandoProperties) {
     props.createdAt = props.createdAt ?? new Date();
     const {name, fatherName, motherName, birthday, sex, createdAt} = props;
-    return new Evangelizando(name, fatherName, motherName, birthday, sex, new UniqueEntityId(), createdAt, createdAt);
+    return new Evangelizando(name, sex, new UniqueEntityId(), createdAt, createdAt, fatherName, motherName, birthday);
   }
 
   delete(deletedAt: Date) {
