2a3a392fe91aa8e27430826aea729e595284d152
Cezar Gabriel Heinen Zaleski <cezar.zaleski@gmail.com>
2022-07-31 17:56:21 -0300

    chore: configure env nestjs module

--- src/nestjs/src/app.controller.ts
+++ src/nestjs/src/app.controller.ts
@@ -1,10 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppService } from './app.service';
 import { Book, PublisherId } from '@geccal/core/collection/domain';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly configService: ConfigService
+  ) {}
 
   @Get()
   getHello(): string {
--- src/nestjs/src/app.module.ts
+++ src/nestjs/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CollectionModule } from './collection/collection.module';
+import { ConfigModule } from 'src/config/config.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [CollectionModule],
+  imports: [CollectionModule, ConfigModule.forRoot()],
 })
 export class AppModule {}
