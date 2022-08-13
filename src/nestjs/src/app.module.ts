992cf6dadbd65b6a982f26d7d77246f3c11e4f26
Cezar Gabriel Heinen Zaleski <cezar.zaleski@gmail.com>
2022-08-13 16:37:28 -0300

    chore: create and configure database module in nestjs

--- src/nestjs/src/app.module.ts
+++ src/nestjs/src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CollectionModule } from './collection/collection.module';
-import { ConfigModule } from 'src/config/config.module';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from './config/config.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [CollectionModule, ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), CollectionModule, DatabaseModule],
 })
 export class AppModule {}
