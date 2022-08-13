5c9110f70039a6a83f4431eb9a69ccb61d4865ca
Cezar Gabriel Heinen Zaleski <cezar.zaleski@gmail.com>
2022-08-13 15:45:30 -0300

    test(book.model.spec): create publisher before create book

--- src/@core/src/collection/infra/db/sequelize/book.model.spec.ts
+++ src/@core/src/collection/infra/db/sequelize/book.model.spec.ts
@@ -1,10 +1,15 @@
 import { BookSequelize } from '#collection/infra/db/sequelize/book.sequelize';
 import { setupSequelize } from '#shared/infra';
+import { PublisherSequelize } from '#collection/infra';
+import { getPublisherPropertiesFake } from '#collection/domain/entities/__tests__/publisherPropertiesFake';
+import { Publisher } from '#collection/domain';
 
 describe('BookModel Unit Test', () => {
-  setupSequelize({models: [BookSequelize.BookModel]})
-
+  setupSequelize({models: [BookSequelize.BookModel, PublisherSequelize.PublisherModel]})
   test('should create new BookModel', async () => {
+    const publisherProps = getPublisherPropertiesFake()
+    const publisher = Publisher.from(publisherProps)
+    await PublisherSequelize.PublisherModel.create(publisher)
     const bookExpected = {
       id: '9366b7dc-2d71-4799-b91c-c64adb205104',
       createdAt: new Date(),
@@ -12,7 +17,7 @@ describe('BookModel Unit Test', () => {
       edition: '1ª',
       name: 'A new Book',
       status: 'disponivel',
-      publisherId: '9366b7dc-2d71-4799-b91c-c64adb205104',
+      publisherId: publisher.id,
       origin: 'doado'
     }
     const subject = await BookSequelize.BookModel.create(bookExpected)
