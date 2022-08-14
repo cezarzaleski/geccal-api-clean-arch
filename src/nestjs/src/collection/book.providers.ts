30cecd61ed365b248a141c2452becb04c503b8e9
Cezar Gabriel Heinen Zaleski <cezar.zaleski@gmail.com>
2022-08-13 22:00:58 -0300

    feat(book.providers): configure repository sqlite

--- src/nestjs/src/collection/book.providers.ts
+++ src/nestjs/src/collection/book.providers.ts
@@ -6,30 +6,40 @@ import {
 } from '@geccal/core/collection/application';
 import { BookRepository } from '@geccal/core/collection/domain';
 import { BookSequelize } from '@geccal/core/collection/infra';
+import { getModelToken } from '@nestjs/sequelize';
 
 export namespace BOOK_PROVIDERS {
   export namespace REPOSITORIES {
     export const BOOK_REPOSITORY_SEQUELIZE = {
       provide: 'BookRepositorySequelize',
-      useClass: BookSequelize.Repository
+      useFactory: (bookModel: typeof BookSequelize.BookModel) => {
+        return new BookSequelize.Repository(bookModel);
+      },
+      inject: [getModelToken(BookSequelize.BookModel)],
+    };
+
+    export const BOOK_REPOSITORY = {
+      provide: 'BookRepository',
+      useExisting: 'BookRepositorySequelize',
     };
   }
 
   export namespace USE_CASES {
-    export const CREATE_CATEGORY_USE_CASE = {
+    export const CREATE_BOOK_USE_CASE = {
       provide: CreateBookUseCase.UseCase,
       useFactory: (categoryRepo: BookRepository.Repository) => {
         return new CreateBookUseCase.UseCase(categoryRepo);
       },
-      inject: [REPOSITORIES.BOOK_REPOSITORY_SEQUELIZE.provide],
+      inject: [REPOSITORIES.BOOK_REPOSITORY.provide],
     };
 
-    export const UPDATE_CATEGORY_USE_CASE = {
+
+    export const UPDATE_BOOK_USE_CASE = {
       provide: UpdateBookUseCase.UseCase,
       useFactory: (categoryRepo: BookRepository.Repository) => {
         return new UpdateBookUseCase.UseCase(categoryRepo);
       },
-      inject: [REPOSITORIES.BOOK_REPOSITORY_SEQUELIZE.provide],
+      inject: [REPOSITORIES.BOOK_REPOSITORY.provide],
     };
   }
 }
