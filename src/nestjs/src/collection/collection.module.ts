5d41f44036c581628cb25c2459cb9acb22c0a810
Cezar Gabriel Heinen Zaleski <cezar.zaleski@gmail.com>
2022-09-11 18:34:51 -0300

    fix(collection.module): organize imports

--- src/nestjs/src/collection/collection.module.ts
+++ src/nestjs/src/collection/collection.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { BookSequelize } from '@geccal/core/collection/infra';
-import { PublisherSequelize } from '@geccal/core/collection/infra';
+import { BookSequelize, PublisherSequelize } from '@geccal/core/collection/infra';
 import { BooksController } from './books.controller';
 import { PublishersController } from './publisher.controller';
 import { BOOK_PROVIDERS } from './book.providers';
@@ -11,10 +10,10 @@ import { PUBLISHER_PROVIDERS } from './publisher.providers';
   imports: [SequelizeModule.forFeature([BookSequelize.BookModel, PublisherSequelize.PublisherModel])],
   controllers: [BooksController, PublishersController],
   providers: [
-    ...Object.values(BOOK_PROVIDERS.REPOSITORIES),
-    ...Object.values(BOOK_PROVIDERS.USE_CASES),
     ...Object.values(PUBLISHER_PROVIDERS.REPOSITORIES),
     ...Object.values(PUBLISHER_PROVIDERS.USE_CASES),
+    ...Object.values(BOOK_PROVIDERS.REPOSITORIES),
+    ...Object.values(BOOK_PROVIDERS.USE_CASES),
   ],
 })
 export class CollectionModule {}
