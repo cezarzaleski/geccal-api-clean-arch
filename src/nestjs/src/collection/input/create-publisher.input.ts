1f351d6b34d3cabe0d7d37aaae2df6e159c55fb7
Cezar Gabriel Heinen Zaleski <cezar.zaleski@gmail.com>
2022-09-11 18:35:55 -0300

    refactor(create-publisher.input): rename field ativo to active

--- src/nestjs/src/collection/input/create-publisher.input.ts
+++ src/nestjs/src/collection/input/create-publisher.input.ts
@@ -3,5 +3,5 @@ import { CreatePublisherUseCase } from '@geccal/core/collection/application';
 
 export class CreatePublisherInput implements CreatePublisherUseCase.Input {
   name: string;
-  ativo?: boolean;
+  active?: boolean;
 }
