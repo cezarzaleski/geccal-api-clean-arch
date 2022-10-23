951ebae2a4c24804a85bd26baa8ed7a7280da480
Cezar Gabriel Heinen Zaleski <cezar.zaleski@gmail.com>
2022-10-23 20:51:27 -0300

    refactor(book): create a book with available

--- src/@core/src/collection/application/use-cases/create-book.use-case.ts
+++ src/@core/src/collection/application/use-cases/create-book.use-case.ts
@@ -2,13 +2,26 @@ import { default as DefaultUseCase } from '#shared/application/use-case';
 import { BookRepository } from '#collection/domain/repository';
 import { Book } from '#collection/domain';
 import { BookOutput, BookOutputMapper } from '../output/book-output';
+import { isEmpty } from '#shared/domain';
+import StatusBook from '#collection/domain/entities/status-book.vo';
 
 export namespace CreateBookUseCase {
   export class UseCase implements DefaultUseCase<Input, Output> {
     constructor(private bookRepository: BookRepository.Repository) {}
 
     async execute(input: Input): Promise<Output> {
-      const entity = Book.from(input);
+      const entity = Book.from({
+        name: input.name,
+        exemplary: input.exemplary,
+        status: StatusBook.AVAILABLE.toString(),
+        edition: input.edition,
+        year: input.year,
+        publisherId: input.publisherId,
+        authors: input.authors,
+        origin: input.origin,
+        createdAt: isEmpty(input.createdAt) ? null : new Date(input.createdAt),
+        updatedAt: isEmpty(input.updatedAt) ? null : new Date(input.updatedAt),
+      });
       await this.bookRepository.insert(entity);
       return BookOutputMapper.toOutput(entity);
     }
@@ -17,13 +30,13 @@ export namespace CreateBookUseCase {
   export type Input = {
     name: string
     exemplary: number
-    status: string
     edition: string
     year: number
     publisherId: string
     authors: Array<string>
     origin: string
-    createdAt: Date
+    createdAt?: string;
+    updatedAt?: string;
   };
 
   export type Output = BookOutput;
